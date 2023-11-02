59b933bb8b17133c5e7cb4773f228a2b26897224
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-02 08:30:15 +0530

    connect to mySql

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const app = require("./app");
+const Sequelize = require("./db/mySql");
+
+connectMySql = async () => {
+  try {
+    await Sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+connectMySql();
 
 
 port = process.env.port || 9800;
