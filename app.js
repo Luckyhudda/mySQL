4e5bf5d5ab631047931a06f8d482ad11b31bcebe
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-02 08:33:25 +0530

    connect route

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const crudRoute = require("./route/route")
+
+app.use('/',crudRoute);
 
 module.exports = app;
